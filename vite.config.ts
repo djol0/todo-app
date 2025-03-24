3e11f3a6b59592c1428e03dc4d3339e2eb41e9a2
Djordje <djordje@Djordjes-Mac-mini.local>
2025-03-24 22:37:15 +0100

    little fix (deploy)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-app",
+  base: "/todo-app/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
