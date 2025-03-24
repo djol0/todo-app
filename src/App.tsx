eb91c052f9b7a902f42615c1f58e3f84cc631cad
Djordje <djordje@Djordjes-Mac-mini.local>
2025-03-24 19:59:42 +0100

    little fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import TodoPage from "./pages/TodoPage";
 import CanbanPage from "./pages/CanbanPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TodoPage />} />
-      <Route path="/todos" element={<CanbanPage />} />
-      
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TodoPage />} />
+        <Route path="/todos" element={<CanbanPage />} />
+      </Routes>
+    </Router>
   );
 }
 
